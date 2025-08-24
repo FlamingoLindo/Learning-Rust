97dc25e7dee28f16a22686f513c73c002f30beb8
FlamingoLindo <vitorantunes2003@gmail.com>
2025-08-24 12:37:29 -0300

    feat: variables exercices

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Learning-Rust
 This a repository to keep track of my learning progress of the programming language Rust
+https://practice.course.rs/
---
+++ learning-rust/src/Variables/7.rs
@@ -0,0 +1,16 @@
+// fn main() {
+//     let x = 1;
+// }
+
+// // Warning: unused variable: `x`
+
+// First solution
+// fn main() {
+//     let _x = 1;
+// }
+
+// Second solution
+fn main() {
+    let x = 1;
+    println!("x is: {}", x);
+}
